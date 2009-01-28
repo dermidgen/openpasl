ee8b76871339c82360a09c6fa7f92e6d51d8a1f2
good.midget <good.midget@38472cee-844d-11dd-8d42-05603b9c63e9>
2009-01-28 01:24:57 +0000

    Added a method for getting all the ACLs on an object
    git-svn-id: https://openpasl.googlecode.com/svn@83 38472cee-844d-11dd-8d42-05603b9c63e9

--- trunk/src/PASL/RBAC/RBAC.php
+++ trunk/src/PASL/RBAC/RBAC.php
@@ -55,9 +55,49 @@ class PASL_RBAC
 		return PASL_ORM::getDB('pasl_rbac');
 	}
 	
+	public function getObjectACLs($type, $uid)
+	{
+		$db = $this->getDB();
+		$query = "select
+			    pr.c_role,
+			    pr.c_who,
+			    case
+			        when (pr.c_role = 'user') then coalesce(us.c_username, '--DNE--')
+			        when (pr.c_role = 'group') then ''
+			        when (pr.c_role = 'owner_group') then ''
+			        else 'none'
+			    end as c_name,
+			    pr.c_action,
+			    pr.c_type,
+			    pr.c_related_table as c_table,
+			    pr.c_related_uid,
+			    ia.c_status
+			from t_privilege as pr
+			    inner join t_action as ac on ac.c_title = pr.c_action
+			    inner join %s as ob on ob.c_uid = %s
+			    inner join t_implemented_action as ia on ia.c_table = '%s'
+			        and ia.c_action = ac.c_title
+			    left outer join t_user as us
+			        on pr.c_role = 'user'
+			        and pr.c_who = us.c_uid
+			where (
+			        (pr.c_type = 'object' and pr.c_related_uid = %s)
+			        or (pr.c_type in ('table', 'global'))
+			        or (pr.c_role = 'self' and pr.c_related_table = 't_user'))
+			    and pr.c_related_table = '%s';";
+		
+		$query = sprintf($query, $type, $uid, $type, $uid, $type);
+
+//		$bind = Array('v_table_name'=>$table,'v_object_id'=>$uid);
+//		$res = $db->fetchAll($db->query($query,$bind));
+		
+		$res = $db->queryAll($query);
+		
+		return var_export($res, true);
+	}
+	
 	public function getPermissions($user_uid, $resource_uid)
 	{
-		
 	}
 	
 	public static function GetInstance()
