d1650c0e77769126cc7c0321fcaec8e569efdf26
Danny Graham <good.midget@gmail.com>
2009-12-28 17:46:42 -0800

    Updated directory structure to remove SVN trailing garbage

--- trunk/lib/phpdoc/phpDoc - ExtJS v0.2/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PWD=pwd
-ln -s $PWD/Extjs/ /usr/share/php/data/PhpDocumentor/phpDocumentor/Converters/HTML/frames/Extjs/
-ln -s $PWD/Extjs/ /usr/share/php/PhpDocumentor/phpDocumentor/Converters/HTML/frames/Extjs/
